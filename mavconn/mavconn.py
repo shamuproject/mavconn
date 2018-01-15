99b9ffc22e032bdde563aee26402d9740cf590f9
Severyn Polakiewicz <severyn.polakiewicz@colorado.edu>
2018-01-15 15:55:01 -0700

    mavconn.py attempted to recreate UML

--- mavconn/mavconn.py
+++ mavconn/mavconn.py
@@ -1,1 +1,17 @@
-from pymavlink import mavutil.mavudp
+from pymavlink.mavutil import mavudp
+
+class MAVLinkConnection:
+'''Definition of MAVCONN class'''
+	def __init__(self, mavfile):
+		self.mav = mavfile
+		
+	def push_handler(self, message, handler):
+	
+	def pop_handler(self, message):
+		'''return function(mav,message)'''
+		
+	def clear_handler(message):
+	
+	def add_timer(period, handler):
+	
+	
