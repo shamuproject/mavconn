ae24f9e83f0d03a8ed4da73930a340cf146abb8c
Severyn Polakiewicz <severyn.polakiewicz@colorado.edu>
2018-01-17 12:49:01 -0700

    Adding Unit Tests mavconn

--- mavconn/mavconn.py
+++ mavconn/mavconn.py
@@ -1,17 +1,21 @@
 from pymavlink.mavutil import mavudp
 
+
 class MAVLinkConnection:
-'''Definition of MAVCONN class'''
-	def __init__(self, mavfile):
-		self.mav = mavfile
-		
-	def push_handler(self, message, handler):
+    """Definition of MAVCONN class"""
+
+    def __init__(self, mavfile):
+        self.mav = mavfile
 	
-	def pop_handler(self, message):
-		'''return function(mav,message)'''
-		
-	def clear_handler(message):
-	
-	def add_timer(period, handler):
-	
-	
+    def push_handler(self, message, handler):
+        pass
+
+    def pop_handler(self, message):
+        """return function(mav,message)"""
+        pass
+
+    def clear_handler(message):
+        pass
+
+    def add_timer(period, handler):
+        pass
---
+++ tests/test_mavconn.py
@@ -0,0 +1,7 @@
+from mavconn.mavconn import MAVLinkConnection
+
+mavfile = 1.0
+
+def test_initialization():
+	test_mav = MAVLinkConnection(mavfile)
+	assert test_mav.mav == 1.0
