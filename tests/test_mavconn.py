ba8fc54a52e22dcd6bf4d5aa79d5ea495b2efa4d
Severyn Polakiewicz <severyn.polakiewicz@colorado.edu>
2018-01-24 20:02:22 -0700

    58 percent coverage mavconn

--- tests/test_mavconn.py
+++ tests/test_mavconn.py
@@ -1,8 +1,13 @@
 from mavconn.mavconn import MAVLinkConnection
 
 mavfile = 1.0
+test_stack = {'HEARTBEAT':['handler1','handler2'],'TELEMETRY':['handler1']}
 
 def test_initialization():
 	test_mav = MAVLinkConnection(mavfile)
 	assert test_mav.mav == 1.0
 	assert test_mav._stacks == {}
+	test_mav.push_handler('HEARTBEAT','handler1')
+	test_mav.push_handler('HEARTBEAT','handler2')
+	test_mav.push_handler('TELEMETRY','handler1')
+	assert test_mav._stacks == test_stack
