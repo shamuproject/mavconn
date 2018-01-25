2b9ca18e3c09a86fe8c992fa3d7d1ac5061f6c20
Severyn Polakiewicz <severyn.polakiewicz@colorado.edu>
2018-01-25 14:55:04 -0700

    88 percent pass

--- tests/test_mavconn.py
+++ tests/test_mavconn.py
@@ -1,3 +1,5 @@
+import pytest
+
 from mavconn.mavconn import MAVLinkConnection
 
 mavfile = 1.0
@@ -6,18 +8,19 @@ test_push = {'HEARTBEAT':['handler1','handler2'],'TELEMETRY':['handler3']}
 test_clear = {}
 
 def test_initialization():
-	test_mav = MAVLinkConnection(mavfile)
-	assert test_mav.mav == 1.0
-	assert test_mav._stacks == {}
-	test_mav.push_handler('HEARTBEAT','handler1')
-	test_mav.push_handler('HEARTBEAT','handler2')
-	test_mav.push_handler('TELEMETRY','handler3')
-	assert test_mav._stacks == test_stack
-	handler_test = test_mav.pop_handler('TELEMETRY')
-	assert handler_test == 'handler3'
-#	unittest.TestCase.assertRaises(test_mav,IndexError,test_mav.pop_handler,'TELEMETRY')
-#	unittest.TestCase.assertRaises(test_mav,KeyError,test_mav.pop_handler,'NOTKEY')
-	test_mav.clear_handler('HEARTBEAT')
-	test_mav.clear_handler('TELEMETRY')
-	assert test_mav._stacks == test_clear  
+    test_mav = MAVLinkConnection(mavfile)
+    assert test_mav.mav == 1.0
+    assert test_mav._stacks == {}
+    test_mav.push_handler('HEARTBEAT','handler1')
+    test_mav.push_handler('HEARTBEAT','handler2')
+    test_mav.push_handler('TELEMETRY','handler3')
+    assert test_mav._stacks == test_stack
+    handler_test = test_mav.pop_handler('TELEMETRY')
+    assert handler_test == 'handler3'
+    with pytest.raises(KeyError, message="That message name key does not exist!"):
+        test_mav.pop_handler('TELEMETRY')
+
+    test_mav.clear_handler('HEARTBEAT')
+    test_mav.clear_handler('TELEMETRY')
+    assert test_mav._stacks == test_clear  
     
